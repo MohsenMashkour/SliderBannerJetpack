651110b9ca226e2c70969fe1e1aab4af74d7578c
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-10-31 10:11:33 +0100

    first init

---
+++ app/src/main/java/com/mkrdeveloper/sliderbannerjetpack/MainActivity.kt
@@ -0,0 +1,190 @@
+package com.mkrdeveloper.sliderbannerjetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import com.mkrdeveloper.sliderbannerjetpack.ui.theme.SliderBannerJetpackTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SliderBannerJetpackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+
+    val images = listOf(
+        R.drawable.p1,
+        R.drawable.p2,
+        R.drawable.p3,
+        R.drawable.p4
+    )
+    val pagerState = rememberPagerState(
+        pageCount =
+        images.size
+    )
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(2000)
+            val nextPage = (pagerState.currentPage + 1) % pagerState.pageCount
+            pagerState.scrollToPage(nextPage)
+        }
+    }
+    val scope = rememberCoroutineScope()
+
+    Column(
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(modifier = modifier.wrapContentSize()) {
+            HorizontalPager(
+                state = pagerState,
+                modifier
+                    .wrapContentSize()
+
+            ) { currentPage ->
+
+                Card(
+                    modifier
+                        .wrapContentSize()
+                        .padding(26.dp),
+                    elevation = CardDefaults.cardElevation(8.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = images[currentPage]),
+                        contentDescription = ""
+                    )
+                }
+            }
+            IconButton(
+                onClick = {
+                    val nextPage = pagerState.currentPage + 1
+                    if (nextPage < images.size) {
+                        scope.launch {
+                            pagerState.scrollToPage(nextPage)
+                        }
+                    }
+                },
+                modifier
+                    .padding(30.dp)
+                    .size(48.dp)
+                    .align(Alignment.CenterEnd)
+                    .clip(CircleShape),
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = Color(0x52373737)
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowRight, contentDescription = "",
+                    modifier.fillMaxSize(),
+                    tint = Color.LightGray
+                )
+            }
+            IconButton(
+                onClick = {
+                    val prevPage = pagerState.currentPage -1
+                    if (prevPage >= 0) {
+                        scope.launch {
+                            pagerState.scrollToPage(prevPage)
+                        }
+                    }
+                },
+                modifier
+                    .padding(30.dp)
+                    .size(48.dp)
+                    .align(Alignment.CenterStart)
+                    .clip(CircleShape),
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = Color(0x52373737)
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowLeft, contentDescription = "",
+                    modifier.fillMaxSize(),
+                    tint = Color.LightGray
+                )
+            }
+        }
+
+        PageIndicator(
+            pageCount = images.size,
+            currentPage = pagerState.currentPage,
+            modifier = modifier
+        )
+
+    }
+}
+
+@Composable
+fun PageIndicator(pageCount: Int, currentPage: Int, modifier: Modifier) {
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+       repeat(pageCount){
+           IndicatorDots(isSelected = it == currentPage, modifier= modifier)
+       }
+    }
+}
+
+@Composable
+fun IndicatorDots(isSelected: Boolean, modifier: Modifier) {
+    val size = animateDpAsState(targetValue = if (isSelected) 12.dp else 10.dp, label = "")
+    Box(modifier = modifier.padding(2.dp)
+        .size(size.value)
+        .clip(CircleShape)
+        .background(if (isSelected) Color(0xff373737) else Color(0xA8373737))
+    )
+}
